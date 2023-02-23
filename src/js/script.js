f9d96c1ddafb060f8edde4fb5c37a9e991ccb49d
jinoby <jinoby17@gmail.com>
2023-02-23 16:05:11 +0300

    add section modal

--- src/js/script.js
+++ src/js/script.js
@@ -66,8 +66,27 @@ $(function (){
     };
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
+
+    //modal
+
+    $('[data-modal=consultation]').on('click', function(){
+        $('.overlay, #consultation') .fadeIn('slow')
+    });
+    $('.modal__close').on('click', function(){
+        $('.overlay, #consultation, #order, #thanks') .fadeOut('slow')
+    })
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function () {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        })
+    });
 });
 
 // document.querySelector('.next').onclick = function () {
 //     slider.goTo("next");
 // };
+
+
+
+
