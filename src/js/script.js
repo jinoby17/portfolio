0646d93c745ddd0a02fe59f84a91fb6ddd214a8e
jinoby <jinoby17@gmail.com>
2023-03-01 22:10:59 +0300

    add validation

--- src/js/script.js
+++ src/js/script.js
@@ -81,6 +81,78 @@ $(function (){
             $('.overlay, #order').fadeIn('slow');
         })
     });
+    function validateForms(form){
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста введите ваше имя",
+                    minlength: jQuery.validator.format("Не меньше {0} символов!")
+                },
+                phone: "пожалуйста введите ваш номер телефона",
+                email: {
+                    required: "Пожалуйста введите вашу электронную почту",
+                    email: "Введите вашу почту в формате: name@domain.com"
+                }
+            }
+
+        });
+
+    };
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
+    
+    $('form').submit(function(e){
+        e.preventDefault();
+
+        if(!$(this).valid()){
+            return;
+        }
+
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+    //scroll and pageup
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else{
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href='#up']").click( function () {
+
+        const _href = $(this).attr("href");
+
+        $("html, body").animate({
+            scrollTop: $(_href).offset().top+"px"
+        });
+        return false;
+    });
+    new WOW().init();
 });
 
 // document.querySelector('.next').onclick = function () {
